a05f11142ad3c89fd5f93878d29bfe7185a4eb93
Coleton Gorecke <coletoncodes@gmail.com>
2023-05-04 20:28:26 -0500

    Added CreateMovie migration, verified working in Postico

--- postgres-example/Sources/App/configure.swift
+++ postgres-example/Sources/App/configure.swift
@@ -4,19 +4,19 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .psql)
-
-    app.migrations.add(CreateTodo())
-
+    
+    app.databases.use(
+        .postgres(
+            hostname: "localhost",
+            username: "postgres",
+            password: "",
+            database: "moviesdb"
+        ),
+        as: .psql
+    )
+    
+    app.migrations.add(CreateMovie(), to: .psql)
+    
     // register routes
     try routes(app)
 }
