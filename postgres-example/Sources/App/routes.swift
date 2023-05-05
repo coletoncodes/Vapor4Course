41702cecf97c7c0eb6ce3849100305184997f258
Coleton Gorecke <coletoncodes@gmail.com>
2023-05-04 21:14:50 -0500

    Added get movie by id

--- postgres-example/Sources/App/routes.swift
+++ postgres-example/Sources/App/routes.swift
@@ -3,13 +3,28 @@ import Vapor
 
 func routes(_ app: Application) throws {
     
-    // movies POST
-    app.post("movies") { req -> EventLoopFuture<Movie> in
+    // /movies
+    app.get("movies") { req async throws -> [Movie] in
+        try await Movie.query(on: req.db).all()
+    }
+    
+    // /movie/id
+    app.get("movie", ":movie_id") { req async throws -> Movie in
+        guard let movie = try await Movie.find(req.parameters.get("movie_id"), on: req.db) else {
+            throw Abort(.notFound, reason: "Invalid or missing movie_id")
+        }
+        
+        return movie
+    }
+    
+    // /movies POST
+    app.post("movies") { req -> Movie in
         // Decode movie
         // Content = body of HTTP request
         let movie = try req.content.decode(Movie.self)
         // After creating in database, return the Model.
-        // This is optional, you could return .ok or whatever else you wanted. 
-        return movie.create(on: req.db).map { movie }
+        // This is optional, you could return .ok or whatever else you wanted.
+        try await movie.create(on: req.db)
+        return movie
     }
 }
