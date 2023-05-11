3b358f8d881ff374a0f48bca370e19e14e4fcc52
Coleton Gorecke <coletoncodes@gmail.com>
2023-05-10 21:14:16 -0500

    added Context

--- hello-leaf/Sources/App/routes.swift
+++ hello-leaf/Sources/App/routes.swift
@@ -1,10 +1,14 @@
 import Vapor
 
+struct Context: Content {
+    let name: String
+}
+
 func routes(_ app: Application) throws {
     
     // Root Route
     app.get { req async throws -> View in
-        try await req.view.render("index")
+        try await req.view.render("index", Context(name: "John Doe"))
     }
     
     // /app
